93d3de973be99aa0a3d3fe713274263e2bca9616
timoha40k <tim.grinch2007@gmail.com>
2026-05-08 19:57:06 +0200

    added icons to weather report

--- src/main.c
+++ src/main.c
@@ -18,7 +18,24 @@
 
 #include "sh1106.h"
 #include "fonts.h"
+#include "wmo_icons.h"
 #include "src/weather_cast.h"
+
+void oled_draw_spritesheet_bitmap(uint8_t x, uint8_t y, uint8_t sprite_width, uint8_t sprite_height,
+   uint8_t spritesheet_width, uint8_t spritesheet_height, uint8_t sprite_num, const uint8_t* spritesheet){
+       uint8_t sheet_cols = spritesheet_width / sprite_width;
+       uint8_t sheet_rows = spritesheet_height / sprite_height;
+
+       uint8_t sprite_col = sprite_num % sheet_cols;
+       uint8_t sprite_row = sprite_num / sheet_cols;
+
+       uint8_t sheet_row_bytes = (spritesheet_width + 7) >> 3;
+       uint8_t sprite_col_bytes = (sprite_col * sprite_width) >> 3;
+        for (uint8_t h = 0; h < sprite_height; h++){
+            uint8_t offset = (sprite_row * sprite_height + h) * sheet_row_bytes + sprite_col_bytes;
+            oled_draw_bitmap(x, y + h, sprite_width, 1, (spritesheet + offset));
+        }
+}
 
 int main(){
     stdio_init_all();
@@ -48,6 +65,8 @@ int main(){
     for (int i = 0; i < 48; i++){
         printf("%d: %.1f\n", i, temperature[i]);
         }
+    float wmo_codes[48];
+    json_get_array_float(server_output.buf, "weather_code", wmo_codes, 48);
 
     sleep_ms(100);
 
@@ -59,9 +78,7 @@ int main(){
 
     sleep_ms(100); // I generally reccomend putting delay, otherwise it might cause strange behaviour
 
-    SH1106* oled = oled_init();
-
-    const uint8_t sunny_32[]  = {
+    /*const uint8_t sunny_32[]  = {
       0x00, 0x00, 0x00, 0x00,
       0x00, 0x02, 0x00, 0x00,
       0x00, 0x84, 0x20, 0x00,
@@ -98,45 +115,44 @@ int main(){
     uint8_t cloud[] = {
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xC0, 0x03, 0x20, 0x04,
       0x20, 0x08, 0x18, 0x10, 0x04, 0x20, 0x02, 0x20, 0x02, 0x20, 0x04, 0x10,
-      0xF8, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-
-    for(uint8_t i = 0; i < 7; i++){
-        oled_draw_rect(i*32, 6, 32, 32, oled);
-    }
-    oled_draw_vline(127, 6, 32, oled);
-    oled_draw_bitmap(0, 7, 31, 32, sunny_32, oled);
-    oled_draw_bitmap(40, 7, 16, 16, cloud, oled);
-    //oled_print_str("4*C", 6, 16, font3x5, &oled);
-    //oled_draw_bitmap(6, 16, 8, 3, ch_a, &oled);
-    //oled_print_ch('~', 3, 16, font5x7, &oled);
-    //oled_print_str("Some text 12349", 5, 40, font5x7, &oled);
-    oled_set_font3x5(oled);
+      0xF8, 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};*/
     //oled_print_float(64.5f, 6, 18, font3x5, &oled);
     struct tm* time;
     run_ntp_once(&time);
-    printf("got ntp response: %02d/%02d/%04d %d:%02d:%02d\n", time->tm_mday, time->tm_mon + 1, time->tm_year + 1900,
-           time->tm_hour, time->tm_min, time->tm_sec);
 
     uint8_t hour_now = time->tm_hour;
     //oled_print_float(25.72, 2, 60, 40, font3x5, &oled);
+    //
+    oled_init();
     //calendar
-    oled_print_str_formating("%02d/%02d/%04d", 0, 0, font3x5, oled, time->tm_mday, time->tm_mon + 1, time->tm_year + 1900);
-    oled_print_str_formating("%02d:%02d", 54, 0, font3x5, oled, hour_now, time->tm_min);
+    oled_set_font3x5();
+    oled_print_str_formating("%02d/%02d/%04d", 0, 0, font3x5, time->tm_mday, time->tm_mon + 1, time->tm_year + 1900);
+    oled_print_str_formating("%02d:%02d", 54, 0, font3x5, hour_now, time->tm_min);
+
+    //drawing boxes
+    for(uint8_t i = 0; i < 4; i++){
+        oled_draw_rect(i*32, 6, 32, 32);
+    }
+
+    oled_draw_vline(127, 6, 32);
 
     // weather information
-    oled_print_str_formating("%d ~C", 8, 25, font3x5, oled, (int)temperature[hour_now]);
-    oled_print_str_formating("%02d:00", 6, 32, font3x5, oled, hour_now);
+    //oled_draw_bitmap(6, 7, WMO_ICONS_WIDTH, WMO_ICONS_HEIGHT, wmo_icons);
+    oled_draw_spritesheet_bitmap(8, 8, 16, 16, WMO_ICONS_WIDTH, WMO_ICONS_HEIGHT, code_to_sprite(wmo_codes[0]), wmo_icons);
+    oled_print_str_formating("%d ~C", 8, 25, font3x5, (int)temperature[hour_now]);
+    oled_print_str_formating("%02d:00", 6, 32, font3x5, hour_now);
 
     uint8_t dt = 0;
     for (int i = 1; i <= 4; i++){
         uint8_t hour_next = hour_now + (i * 6);
 
         dt = (hour_next >= 24) ? 24 : 0;
-        oled_print_str_formating("%d ~C", 8 + (32 * i), 25, font3x5, oled, (int)temperature[hour_next]);
-        oled_print_str_formating("%02d:00", 6 + (32 * i), 32, font3x5, oled, hour_next - dt);
+        oled_print_str_formating("%d ~C", 8 + (32 * i), 25, font3x5, (int)temperature[hour_next]);
+        oled_print_str_formating("%02d:00", 6 + (32 * i), 32, font3x5, hour_next - dt);
+        oled_draw_spritesheet_bitmap(8 + (32 * i), 8, 16, 16, WMO_ICONS_WIDTH, WMO_ICONS_HEIGHT, code_to_sprite((uint8_t)wmo_codes[hour_next]), wmo_icons);
     }
 
-    oled_update_screen(oled);
+    oled_update_screen();
     while(1) sleep_ms(2000);
     //for simplicity sake we'll assume that temperature[i] is temperature at i hour temperture[1] -> 01:00 etc
 
